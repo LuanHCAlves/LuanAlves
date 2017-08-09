76eaa49ec6367eb2e0af2a498dc052a9b407f4e9
Lab. Desenvolvimento <Lab. Desenvolvimento@RB-LAB102-29>
2017-08-08 21:57:49 -0300

    Add project files.

---
+++ LuanAlves/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using LuanAlves.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuanAlves.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private static IList<Category> categoryList =
+            new List<Category>()
+            {
+        new Category() { CategoryId = 1, Name = "Keyboard" },
+        new Category() { CategoryId = 2, Name = "Monitor" },
+        new Category() { CategoryId = 3, Name = "Laptop" },
+        new Category() { CategoryId = 4, Name = "Mouse" },
+        new Category() { CategoryId = 5, Name = "Printer" },
+
+    };
+            
+            // GET: Categories
+        public ActionResult Index()
+        {
+            return View(categoryList.OrderBy(c => c.Name));
+        }
+    }
+}
---
+++ LuanAlves/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LuanAlves.MvcApplication" Language="C#" %>
